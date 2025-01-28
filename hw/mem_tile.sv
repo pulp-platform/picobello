f9d95872827833b739937f9b743b35f98e038e30
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-01-28 10:36:39 +0100

    hw(mem): Join narrow and wide AXI in mem tile

--- hw/mem_tile.sv
+++ hw/mem_tile.sv
@@ -5,6 +5,7 @@
 // Author: Tim Fischer <fischeti@iis.ee.ethz.ch>
 
 `include "common_cells/registers.svh"
+`include "axi/typedef.svh"
 
 module mem_tile
   import floo_pkg::*;
@@ -24,122 +25,58 @@ module mem_tile
   input  floo_wide_t [West:North]   floo_wide_i
 );
 
-  localparam int unsigned NarrowNumWords = 4096; // 128kB
-  localparam int unsigned WideNumWords = 16384; // 1MB
+  localparam int unsigned MemSize = 1024 * 1024; // 1MB
+  localparam int unsigned MemNumWords = MemSize / (AxiCfgW.DataWidth / 8);
 
-  typedef logic [$clog2(NarrowNumWords)-1:0] narrow_mem_addr_t;
-  typedef logic [AxiCfgN.DataWidth-1:0] narrow_mem_data_t;
-  typedef logic [AxiCfgN.DataWidth/8-1:0] narrow_mem_be_t;
+  ////////////
+  // Router //
+  ////////////
 
-  typedef logic [$clog2(WideNumWords)-1:0] wide_mem_addr_t;
-  typedef logic [AxiCfgW.DataWidth-1:0] wide_mem_data_t;
-  typedef logic [AxiCfgW.DataWidth/8-1:0] wide_mem_be_t;
+  floo_req_t  [Eject:North] router_floo_req_out, router_floo_req_in;
+  floo_rsp_t  [Eject:North] router_floo_rsp_out, router_floo_rsp_in;
+  floo_wide_t [Eject:North] router_floo_wide_out, router_floo_wide_in;
 
-
-  logic narrow_mem_req, narrow_mem_req_q;
-  logic narrow_mem_we;
-  narrow_mem_addr_t narrow_mem_addr;
-  narrow_mem_data_t narrow_mem_wdata;
-  narrow_mem_be_t narrow_mem_be;
-  narrow_mem_data_t narrow_mem_rdata;
-
-  logic wide_mem_req, wide_mem_req_q;
-  logic wide_mem_we;
-  wide_mem_addr_t wide_mem_addr;
-  wide_mem_data_t wide_mem_wdata;
-  wide_mem_be_t wide_mem_be;
-  wide_mem_data_t wide_mem_rdata;
-
-  tc_sram #(
-    .NumWords   (NarrowNumWords), // 128kB
-    .DataWidth  (AxiCfgN.DataWidth),
-    .NumPorts   (1)
-  ) i_narrow_mem (
+  floo_nw_router #(
+    .AxiCfgN      ( AxiCfgN             ),
+    .AxiCfgW      ( AxiCfgW             ),
+    .RouteAlgo    ( RouteCfg.RouteAlgo  ),
+    .NumRoutes    ( 5                   ),
+    .InFifoDepth  ( 2                   ),
+    .OutFifoDepth ( 2                   ),
+    .id_t         ( id_t                ),
+    .hdr_t        ( hdr_t               ),
+    .floo_req_t   ( floo_req_t          ),
+    .floo_rsp_t   ( floo_rsp_t          ),
+    .floo_wide_t  ( floo_wide_t         )
+  ) i_router (
     .clk_i,
     .rst_ni,
-    .req_i  (narrow_mem_req),
-    .we_i   (narrow_mem_we),
-    .addr_i (narrow_mem_addr),
-    .wdata_i(narrow_mem_wdata),
-    .be_i   (narrow_mem_be),
-    .rdata_o(narrow_mem_rdata)
+    .test_enable_i,
+    .id_i,
+    .id_route_map_i ( '0                    ),
+    .floo_req_i     ( router_floo_req_in    ),
+    .floo_rsp_o     ( router_floo_rsp_out   ),
+    .floo_req_o     ( router_floo_req_out   ),
+    .floo_rsp_i     ( router_floo_rsp_in    ),
+    .floo_wide_i    ( router_floo_wide_in   ),
+    .floo_wide_o    ( router_floo_wide_out  )
   );
 
-  tc_sram #(
-    .NumWords   (WideNumWords), // 128kB
-    .DataWidth  (AxiCfgW.DataWidth),
-    .NumPorts   (1)
-  ) i_wide_mem (
-    .clk_i,
-    .rst_ni,
-    .req_i  (wide_mem_req),
-    .we_i   (wide_mem_we),
-    .addr_i (wide_mem_addr),
-    .wdata_i(wide_mem_wdata),
-    .be_i   (wide_mem_be),
-    .rdata_o(wide_mem_rdata)
-  );
+  assign floo_req_o = router_floo_req_out[West:North];
+  assign router_floo_req_in[West:North] = floo_req_i;
+  assign floo_rsp_o = router_floo_rsp_out[West:North];
+  assign router_floo_rsp_in[West:North] = floo_rsp_i;
+  assign floo_wide_o = router_floo_wide_out[West:North];
+  assign router_floo_wide_in[West:North] = floo_wide_i;
+
+  /////////////
+  // Chimney //
+  /////////////
 
   floo_picobello_noc_pkg::axi_narrow_out_req_t axi_narrow_req;
   floo_picobello_noc_pkg::axi_narrow_out_rsp_t axi_narrow_rsp;
   floo_picobello_noc_pkg::axi_wide_out_req_t   axi_wide_req;
   floo_picobello_noc_pkg::axi_wide_out_rsp_t   axi_wide_rsp;
-
-  // TODO: filter ATOPs
-  axi_to_mem #(
-    .AddrWidth   ($clog2(NarrowNumWords)),
-    .DataWidth   (AxiCfgN.DataWidth),
-    .IdWidth     (AxiCfgN.OutIdWidth),
-    .NumBanks    (1),
-    .axi_req_t   (axi_narrow_out_req_t),
-    .axi_resp_t  (axi_narrow_out_rsp_t)
-  ) i_narrow_axi_to_mem (
-    .clk_i,
-    .rst_ni,
-    .busy_o      (),
-    .axi_req_i   (axi_narrow_req),
-    .axi_resp_o  (axi_narrow_rsp),
-    .mem_req_o   (narrow_mem_req),
-    .mem_gnt_i   (1'b1),
-    .mem_addr_o  (narrow_mem_addr),
-    .mem_wdata_o (narrow_mem_wdata),
-    .mem_strb_o  (narrow_mem_be),
-    .mem_atop_o  (), // We need to filter ATOPs previously
-    .mem_we_o    (narrow_mem_we),
-    .mem_rvalid_i(narrow_mem_req_q),
-    .mem_rdata_i (narrow_mem_rdata)
-  );
-
-  axi_to_mem #(
-    .AddrWidth   ($clog2(WideNumWords)),
-    .DataWidth   (AxiCfgW.DataWidth),
-    .IdWidth     (AxiCfgW.OutIdWidth),
-    .NumBanks    (1),
-    .axi_req_t   (axi_wide_out_req_t),
-    .axi_resp_t  (axi_wide_out_rsp_t)
-  ) i_wide_axi_to_mem (
-    .clk_i,
-    .rst_ni,
-    .busy_o      (),
-    .axi_req_i   (axi_wide_req),
-    .axi_resp_o  (axi_wide_rsp),
-    .mem_req_o   (wide_mem_req),
-    .mem_gnt_i   (1'b1),
-    .mem_addr_o  (wide_mem_addr),
-    .mem_wdata_o (wide_mem_wdata),
-    .mem_strb_o  (wide_mem_be),
-    .mem_atop_o  (), // No atops on wide
-    .mem_we_o    (wide_mem_we),
-    .mem_rvalid_i(wide_mem_req_q),
-    .mem_rdata_i (wide_mem_rdata)
-  );
-
-  `FF(narrow_mem_req_q, narrow_mem_req, '0)
-  `FF(wide_mem_req_q, wide_mem_req, '0)
-
-  floo_req_t  [Eject:North] router_floo_req_out, router_floo_req_in;
-  floo_rsp_t  [Eject:North] router_floo_rsp_out, router_floo_rsp_in;
-  floo_wide_t [Eject:North] router_floo_wide_out, router_floo_wide_in;
 
   floo_nw_chimney #(
     .AxiCfgN              ( AxiCfgN                                   ),
@@ -188,37 +125,102 @@ module mem_tile
     .floo_wide_i          ( router_floo_wide_out[Eject] )
   );
 
-  floo_nw_router #(
-    .AxiCfgN      ( AxiCfgN             ),
-    .AxiCfgW      ( AxiCfgW             ),
-    .RouteAlgo    ( RouteCfg.RouteAlgo  ),
-    .NumRoutes    ( 5                   ),
-    .InFifoDepth  ( 2                   ),
-    .OutFifoDepth ( 2                   ),
-    .id_t         ( id_t                ),
-    .hdr_t        ( hdr_t               ),
-    .floo_req_t   ( floo_req_t          ),
-    .floo_rsp_t   ( floo_rsp_t          ),
-    .floo_wide_t  ( floo_wide_t         )
-  ) i_router (
-    .clk_i,
-    .rst_ni,
-    .test_enable_i,
-    .id_i,
-    .id_route_map_i ( '0                    ),
-    .floo_req_i     ( router_floo_req_in    ),
-    .floo_rsp_o     ( router_floo_rsp_out   ),
-    .floo_req_o     ( router_floo_req_out   ),
-    .floo_rsp_i     ( router_floo_rsp_in    ),
-    .floo_wide_i    ( router_floo_wide_in   ),
-    .floo_wide_o    ( router_floo_wide_out  )
+  /////////////
+  // NW Join //
+  /////////////
+
+  localparam axi_cfg_t AxiCfgJoin = floo_pkg::axi_join_cfg(AxiCfgN, AxiCfgW);
+
+  typedef logic [AxiCfgJoin.OutIdWidth-1:0] nw_join_id_t;
+  typedef logic [AxiCfgJoin.UserWidth-1:0] nw_join_user_t;
+
+  `AXI_TYPEDEF_ALL_CT(axi_nw_join, axi_nw_join_req_t, axi_nw_join_rsp_t, axi_wide_out_addr_t,
+       nw_join_id_t, axi_wide_out_data_t, axi_wide_out_strb_t, nw_join_user_t)
+
+  axi_nw_join_req_t axi_req;
+  axi_nw_join_rsp_t axi_rsp;
+
+  floo_nw_join #(
+    .AxiCfgN          ( axi_cfg_swap_iw(AxiCfgN)    ),
+    .AxiCfgW          ( axi_cfg_swap_iw(AxiCfgW)    ),
+    .AxiCfgJoin       ( axi_cfg_swap_iw(AxiCfgJoin) ),
+    .AtopUserAsId     ( 1'b1 ),
+    .axi_narrow_req_t ( axi_narrow_out_req_t ),
+    .axi_narrow_rsp_t ( axi_narrow_out_rsp_t ),
+    .axi_wide_req_t   ( axi_wide_out_req_t   ),
+    .axi_wide_rsp_t   ( axi_wide_out_rsp_t   ),
+    .axi_req_t        ( axi_nw_join_req_t    ),
+    .axi_rsp_t        ( axi_nw_join_rsp_t    )
+  ) i_floo_nw_join (
+    .clk_i            ( clk_i  ),
+    .rst_ni           ( rst_ni ),
+    .test_enable_i    ( test_enable_i  ),
+    .axi_narrow_req_i ( axi_narrow_req ),
+    .axi_narrow_rsp_o ( axi_narrow_rsp ),
+    .axi_wide_req_i   ( axi_wide_req   ),
+    .axi_wide_rsp_o   ( axi_wide_rsp   ),
+    .axi_req_o        ( axi_req ),
+    .axi_rsp_i        ( axi_rsp )
   );
 
-  assign floo_req_o = router_floo_req_out[West:North];
-  assign router_floo_req_in[West:North] = floo_req_i;
-  assign floo_rsp_o = router_floo_rsp_out[West:North];
-  assign router_floo_rsp_in[West:North] = floo_rsp_i;
-  assign floo_wide_o = router_floo_wide_out[West:North];
-  assign router_floo_wide_in[West:North] = floo_wide_i;
+  ///////////////////////
+  // axi2mem converter //
+  ///////////////////////
+
+  typedef logic [$clog2(MemNumWords)-1:0] mem_addr_t;
+  typedef logic [AxiCfgW.DataWidth-1:0] mem_data_t;
+  typedef logic [AxiCfgW.DataWidth/8-1:0] mem_be_t;
+
+  logic mem_req, mem_req_q;
+  logic mem_we;
+  mem_addr_t mem_addr;
+  mem_data_t mem_wdata;
+  mem_be_t mem_be;
+  mem_data_t mem_rdata;
+
+  axi_to_mem #(
+    .AddrWidth  ( $clog2(MemNumWords)   ),
+    .DataWidth  ( AxiCfgJoin.DataWidth  ),
+    .IdWidth    ( AxiCfgJoin.OutIdWidth ),
+    .NumBanks   ( 1 ),
+    .axi_req_t  ( axi_nw_join_req_t ),
+    .axi_resp_t ( axi_nw_join_rsp_t )
+  ) i_axi_to_mem (
+    .clk_i,
+    .rst_ni,
+    .busy_o       ( ),
+    .axi_req_i    ( axi_req ),
+    .axi_resp_o   ( axi_rsp ),
+    .mem_req_o    ( mem_req ),
+    .mem_gnt_i    ( 1'b1    ),
+    .mem_addr_o   ( mem_addr  ),
+    .mem_wdata_o  ( mem_wdata ),
+    .mem_strb_o   ( mem_be    ),
+    .mem_atop_o   ( ), // No atops on wide
+    .mem_we_o     ( mem_we    ),
+    .mem_rvalid_i ( mem_req_q ),
+    .mem_rdata_i  ( mem_rdata )
+  );
+
+  `FF(mem_req_q, mem_req, '0)
+
+  /////////////////
+  // SRAM macros //
+  /////////////////
+
+  tc_sram #(
+    .NumWords  ( MemNumWords          ),
+    .DataWidth ( AxiCfgJoin.DataWidth ),
+    .NumPorts  ( 1 )
+  ) i_mem (
+    .clk_i,
+    .rst_ni,
+    .req_i   ( mem_req   ),
+    .we_i    ( mem_we    ),
+    .addr_i  ( mem_addr  ),
+    .wdata_i ( mem_wdata ),
+    .be_i    ( mem_be    ),
+    .rdata_o ( mem_rdata )
+  );
 
 endmodule
