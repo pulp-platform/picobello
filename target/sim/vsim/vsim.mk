f9f56c50ce9dbf7fc9a345ce70dcf6868891b75a
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-01-31 14:31:17 +0100

    vsim: Log all for GUI target

--- target/sim/vsim/vsim.mk
+++ target/sim/vsim/vsim.mk
@@ -45,7 +45,7 @@ $(VSIM_DIR)/compile.tcl:
 	echo 'vlog -work $(VSIM_WORK) "$(realpath $(CHS_ROOT))/target/sim/src/elfloader.cpp" -ccflags "-std=c++11"' >> $@
 
 vsim-run:
-	$(VSIM) $(VSIM_FLAGS) $(VSIM_FLAGS_GUI) $(TB_DUT)
+	$(VSIM) $(VSIM_FLAGS) $(VSIM_FLAGS_GUI) $(TB_DUT) -do "log -r /*"
 
 vsim-run-batch:
 	$(VSIM) -c $(VSIM_FLAGS) $(TB_DUT) -do "run -all; quit"
