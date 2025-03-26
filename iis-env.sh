eea71f2f9fe5105dc44719f3e1bbdd11852ae03e
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-03-26 19:20:17 +0100

    env: Add `iis-env.sh` script

---
+++ iis-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+
+export VSIM="questa-2023.4 vsim"
+export PYTHON=/usr/local/anaconda3/bin/python3.11
+export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
+export LLVM_BINROOT=/usr/pack/riscv-1.0-kgf/pulp-llvm-0.12.0/bin
