1e60e2ce7e2799f57833b1f30243b0dfbb2bf554
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-03-27 14:45:25 +0100

    env: Activate venv if it exists

--- iis-env.sh
+++ iis-env.sh
@@ -7,3 +7,7 @@ export VSIM="questa-2023.4 vsim"
 export BASE_PYTHON=/usr/local/anaconda3/bin/python3.11
 export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
 export LLVM_BINROOT=/usr/pack/riscv-1.0-kgf/pulp-llvm-0.12.0/bin
+
+if [ -d ".venv" ]; then
+  source .venv/bin/activate
+fi
