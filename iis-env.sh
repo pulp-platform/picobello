54b2ba065dc86ba9cc6f0c8b4caa3885934c26a9
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-04-08 16:36:26 +0200

    env: Use specific oseda version

--- iis-env.sh
+++ iis-env.sh
@@ -7,7 +7,7 @@ export VSIM="questa-2023.4 vsim"
 export BASE_PYTHON=/usr/local/anaconda3/bin/python3.11
 export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
 export LLVM_BINROOT=/usr/pack/riscv-1.0-kgf/pulp-llvm-0.12.0/bin
-export VERIBLE_FMT="oseda verible-verilog-format"
+export VERIBLE_FMT="oseda -2025.03 verible-verilog-format"
 
 # Create the python venv
 if [ ! -d ".venv" ]; then
