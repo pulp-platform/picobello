0f8edfef6c2e11073b0d4d3d28ee99b762efd2bb
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-04-08 10:22:37 +0200

    env: Add `VERIBLE_FMT` variable

--- iis-env.sh
+++ iis-env.sh
@@ -7,6 +7,7 @@ export VSIM="questa-2023.4 vsim"
 export BASE_PYTHON=/usr/local/anaconda3/bin/python3.11
 export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
 export LLVM_BINROOT=/usr/pack/riscv-1.0-kgf/pulp-llvm-0.12.0/bin
+export VERIBLE_FMT="oseda verible-verilog-format"
 
 # Create the python venv
 if [ ! -d ".venv" ]; then
