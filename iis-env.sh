3ef723ed8f015d8a04299fe856e96b3b99481cd5
Andrea Belano <andrea.belano@studio.unibo.it>
2025-06-24 18:41:08 +0200

    [env] set VOPT variable

--- iis-env.sh
+++ iis-env.sh
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: SHL-0.51
 
 export VSIM="questa-2023.4 vsim"
+export VOPT="questa-2023.4 vopt"
 export BASE_PYTHON=/usr/local/anaconda3/bin/python3.11
 export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
 export LLVM_BINROOT=/usr/scratch2/vulcano/colluca/tools/riscv32-snitch-llvm-almalinux8-15.0.0-snitch-0.2.0/bin
