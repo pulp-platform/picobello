b233c74d49a55e41725f35f4a3b160291c170b56
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-06-30 19:46:09 +0200

    sw(test): Fix indexing into clk enable and reset registers

--- sw/cheshire/tests/access_clk_gating_rst_ctrl_reg.c
+++ sw/cheshire/tests/access_clk_gating_rst_ctrl_reg.c
@@ -13,49 +13,49 @@ int main(void) {
     volatile pb_soc_regs_t *pb_soc_regs = &picobello_addrmap.cheshire_internal.pb_soc_regs;
 
     // Write all 0s and check
-    pb_soc_regs->cluster_clk_enables = 0x00000000;
-    pb_soc_regs->mem_tile_clk_enables = 0x00000000;
-    pb_soc_regs->fhg_spu_clk_enables = 0x00000000;
-    pb_soc_regs->cluster_rsts = 0x00000000;
-    pb_soc_regs->mem_tile_rsts = 0x00000000;
-    pb_soc_regs->fhg_spu_rsts = 0x00000000;
+    pb_soc_regs->cluster_clk_enables.f.clk_en = 0x00000000;
+    pb_soc_regs->mem_tile_clk_enables.f.clk_en = 0x00000000;
+    pb_soc_regs->fhg_spu_clk_enables.f.clk_en = 0x00000000;
+    pb_soc_regs->cluster_rsts.f.rst = 0x00000000;
+    pb_soc_regs->mem_tile_rsts.f.rst = 0x00000000;
+    pb_soc_regs->fhg_spu_rsts.f.rst = 0x00000000;
 
-    n_errors -= (pb_soc_regs->cluster_clk_enables == 0x00000000);
-    n_errors -= (pb_soc_regs->mem_tile_clk_enables == 0x00000000);
-    n_errors -= (pb_soc_regs->fhg_spu_clk_enables == 0x00000000);
-    n_errors -= (pb_soc_regs->cluster_rsts == 0x00000000);
-    n_errors -= (pb_soc_regs->mem_tile_rsts == 0x00000000);
-    n_errors -= (pb_soc_regs->fhg_spu_rsts == 0x00000000);
-
-    // Write all 1s and check again
-    pb_soc_regs->cluster_clk_enables = 0x0000FFFF;
-    pb_soc_regs->mem_tile_clk_enables = 0x000000FF;
-    pb_soc_regs->fhg_spu_clk_enables = 0x00000001;
-    pb_soc_regs->cluster_rsts = 0x0000FFFF;
-    pb_soc_regs->mem_tile_rsts = 0x000000FF;
-    pb_soc_regs->fhg_spu_rsts = 0x00000001;
-
-    n_errors -= (pb_soc_regs->cluster_clk_enables == 0x0000FFFF);
-    n_errors -= (pb_soc_regs->mem_tile_clk_enables == 0x000000FF);
-    n_errors -= (pb_soc_regs->fhg_spu_clk_enables == 0x00000001);
-    n_errors -= (pb_soc_regs->cluster_rsts == 0x0000FFFF);
-    n_errors -= (pb_soc_regs->mem_tile_rsts == 0x000000FF);
-    n_errors -= (pb_soc_regs->fhg_spu_rsts == 0x00000001);
+    n_errors -= (pb_soc_regs->cluster_clk_enables.f.clk_en == 0x00000000);
+    n_errors -= (pb_soc_regs->mem_tile_clk_enables.f.clk_en == 0x00000000);
+    n_errors -= (pb_soc_regs->fhg_spu_clk_enables.f.clk_en == 0x00000000);
+    n_errors -= (pb_soc_regs->cluster_rsts.f.rst == 0x00000000);
+    n_errors -= (pb_soc_regs->mem_tile_rsts.f.rst == 0x00000000);
+    n_errors -= (pb_soc_regs->fhg_spu_rsts.f.rst == 0x00000000);
 
     // Write all 1s and check again
-    pb_soc_regs->cluster_clk_enables = 0x0000AAAA;
-    pb_soc_regs->mem_tile_clk_enables = 0x000000AA;
-    pb_soc_regs->fhg_spu_clk_enables = 0x00000000;
-    pb_soc_regs->cluster_rsts = 0x00005555;
-    pb_soc_regs->mem_tile_rsts = 0x00000055;
-    pb_soc_regs->fhg_spu_rsts = 0x00000000;
+    pb_soc_regs->cluster_clk_enables.f.clk_en = 0x0000FFFF;
+    pb_soc_regs->mem_tile_clk_enables.f.clk_en = 0x000000FF;
+    pb_soc_regs->fhg_spu_clk_enables.f.clk_en = 0x00000001;
+    pb_soc_regs->cluster_rsts.f.rst = 0x0000FFFF;
+    pb_soc_regs->mem_tile_rsts.f.rst = 0x000000FF;
+    pb_soc_regs->fhg_spu_rsts.f.rst = 0x00000001;
 
-    n_errors -= (pb_soc_regs->cluster_clk_enables == 0x0000AAAA);
-    n_errors -= (pb_soc_regs->mem_tile_clk_enables == 0x000000AA);
-    n_errors -= (pb_soc_regs->fhg_spu_clk_enables == 0x00000000);
-    n_errors -= (pb_soc_regs->cluster_rsts == 0x00005555);
-    n_errors -= (pb_soc_regs->mem_tile_rsts == 0x00000055);
-    n_errors -= (pb_soc_regs->fhg_spu_rsts == 0x00000000);
+    n_errors -= (pb_soc_regs->cluster_clk_enables.f.clk_en == 0x0000FFFF);
+    n_errors -= (pb_soc_regs->mem_tile_clk_enables.f.clk_en == 0x000000FF);
+    n_errors -= (pb_soc_regs->fhg_spu_clk_enables.f.clk_en == 0x00000001);
+    n_errors -= (pb_soc_regs->cluster_rsts.f.rst == 0x0000FFFF);
+    n_errors -= (pb_soc_regs->mem_tile_rsts.f.rst == 0x000000FF);
+    n_errors -= (pb_soc_regs->fhg_spu_rsts.f.rst == 0x00000001);
+
+    // Write all 1s and check again
+    pb_soc_regs->cluster_clk_enables.f.clk_en = 0x0000AAAA;
+    pb_soc_regs->mem_tile_clk_enables.f.clk_en = 0x000000AA;
+    pb_soc_regs->fhg_spu_clk_enables.f.clk_en = 0x00000000;
+    pb_soc_regs->cluster_rsts.f.rst = 0x00005555;
+    pb_soc_regs->mem_tile_rsts.f.rst = 0x00000055;
+    pb_soc_regs->fhg_spu_rsts.f.rst = 0x00000000;
+
+    n_errors -= (pb_soc_regs->cluster_clk_enables.f.clk_en == 0x0000AAAA);
+    n_errors -= (pb_soc_regs->mem_tile_clk_enables.f.clk_en == 0x000000AA);
+    n_errors -= (pb_soc_regs->fhg_spu_clk_enables.f.clk_en == 0x00000000);
+    n_errors -= (pb_soc_regs->cluster_rsts.f.rst == 0x00005555);
+    n_errors -= (pb_soc_regs->mem_tile_rsts.f.rst == 0x00000055);
+    n_errors -= (pb_soc_regs->fhg_spu_rsts.f.rst == 0x00000000);
 
     return n_errors;
 }
