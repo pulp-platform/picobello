46a34b0b6549f75bba410d09e3b72782f2f9b33d
Luca Colagrande <luca.colagrande3@gmail.com>
2025-10-08 13:47:52 +0200

    sw: Add `pb_team` and `pb_sync` functions

---
+++ sw/snitch/runtime/src/pb_sync.c
@@ -0,0 +1,6 @@
+// Copyright 2025 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+extern inline void pb_create_mesh_comm(snrt_comm_t *comm, uint32_t n_rows,
+    uint32_t n_cols);
