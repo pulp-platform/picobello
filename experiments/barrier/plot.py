d610358cb797d2e1993e50fa06c56a9bc2dde3b8
Luca Colagrande <luca.colagrande3@gmail.com>
2025-10-09 18:29:53 +0200

    experiments: Add barrier experiments

---
+++ experiments/barrier/plot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Luca Colagrande <colluca@iis.ee.ethz.ch>
+
+import argparse
+import matplotlib.pyplot as plt
+from barrier import experiments
+
+
+def plot1(show=True):
+    """Plot runtime vs number of clusters."""
+
+    # Load results
+    df = experiments.results()
+
+    # Get actual runtimes
+    df['cycles'] = df['results'].apply(experiments.get_total_cycles)
+
+    # Plot
+    ax = df.pivot(index="n_clusters", columns="impl", values="cycles").plot(kind="bar")
+    ax.set_xlabel("Nr. clusters")
+    ax.set_ylabel("Runtime [cycles]")
+    ax.tick_params(axis='x', labelrotation=0)
+    ax.grid(axis="y", alpha=0.4)
+    ax.legend()
+    plt.tight_layout()
+    if show:
+        plt.show()
+
+
+def main():
+
+    # Parse arguments
+    functions = [plot1]
+    parser = argparse.ArgumentParser(description='Plot wide multicast results.')
+    parser.add_argument(
+        'plots',
+        nargs='*',
+        default='all',
+        choices=[f.__name__ for f in functions] + ['all'],
+        help='Plots to generate.'
+    )
+    args = parser.parse_args()
+
+    # Helper function to check if a plot was requested on the command line
+    def requested(plot):
+        return plot in args.plots or 'all' in args.plots
+
+    # Generate plots
+    if requested('plot1'):
+        plot1()
+
+
+if __name__ == "__main__":
+    main()
---
+++ experiments/barrier/roi.json.tpl
@@ -0,0 +1,27 @@
+<%
+if experiment['n_clusters'] == 2:
+    clusters = [0, 4]
+elif experiment['n_clusters'] == 4:
+    clusters = [0, 4, 8, 12]
+elif experiment['n_clusters'] == 8:
+    clusters = [0, 4, 8, 12, 1, 5, 9, 13]
+elif experiment['n_clusters'] == 16:
+    clusters = list(range(16))
+%>
+
+[
+% for cluster in clusters:
+    {
+        "thread": "${f'hart_{cluster * 9 + 8 + 1}'}",
+        "roi": [
+    % if experiment['mode'] == 'sw':
+            {"idx": 1, "label": "barrier"},
+            {"idx": 3, "label": "barrier"},
+    % elif experiment['mode'] == 'hw':
+            {"idx": 3, "label": "barrier"},
+            {"idx": 5, "label": "barrier"},
+    % endif
+        ]
+    },
+% endfor
+]
