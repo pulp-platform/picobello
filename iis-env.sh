e54974350622075d82658a158e842ba58a23720c
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-10-23 09:27:45 +0200

    env: Adapt new snitch uv environment

--- iis-env.sh
+++ iis-env.sh
@@ -10,7 +10,8 @@ export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
 export VERIBLE_FMT="oseda -2025.03 verible-verilog-format"
 export SN_LLVM_BINROOT=/usr/scratch2/vulcano/colluca/tools/riscv32-snitch-llvm-almalinux8-15.0.0-snitch-0.2.0/bin
 
-export UV="/home/fischeti/.local/bin/uv"
-export FLOO_GEN="$UV run floogen"
-export PEAKRDL="$UV run peakrdl"
-export PYTHON="$UV run python"
+export UV="/home/fischeti/.local/bin/uv run"
+export PYTHON="$UV"
+export FLOO_GEN="$UV floogen"
+export PEAKRDL="$UV peakrdl"
+export UV_LINK_MODE=copy
