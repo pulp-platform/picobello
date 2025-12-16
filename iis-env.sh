79319b2ba07f61f93336a7f67ad42daa7dff577f
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-12-16 14:33:56 +0100

    env: Bump snitch compiler

--- iis-env.sh
+++ iis-env.sh
@@ -8,7 +8,7 @@ export VOPT="questa-2023.4 vopt"
 export VLIB="questa-2023.4 vlib"
 export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
 export VERIBLE_FMT="oseda -2025.03 verible-verilog-format"
-export SN_LLVM_BINROOT=/usr/scratch2/vulcano/colluca/tools/riscv32-snitch-llvm-almalinux8-15.0.0-snitch-0.2.0/bin
+export SN_LLVM_BINROOT=/usr/scratch2/vulcano/colluca/tools/riscv32-snitch-llvm-almalinux8-15.0.0-snitch-0.5.0/bin
 
 export UV="/usr/local/uv/uv run"
 export UV_LINK_MODE=copy
