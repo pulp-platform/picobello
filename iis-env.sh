30ef095af44c97574651656471bf1f4348399746
Tim Fischer <fischeti@iis.ee.ethz.ch>
2026-02-24 19:56:23 +0100

    iis-env.sh: Update tool versions

--- iis-env.sh
+++ iis-env.sh
@@ -3,9 +3,9 @@
 # Solderpad Hardware License, Version 0.51, see LICENSE for details.
 # SPDX-License-Identifier: SHL-0.51
 
-export VSIM="questa-2023.4 vsim"
-export VOPT="questa-2023.4 vopt"
-export VLIB="questa-2023.4 vlib"
+export VSIM="questa-2025.3 vsim"
+export VOPT="questa-2025.3 vopt"
+export VLIB="questa-2025.3 vlib"
 export CHS_SW_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
 export VERIBLE_FMT="oseda -2025.03 verible-verilog-format"
 export SN_LLVM_BINROOT=/usr/scratch2/vulcano/colluca/tools/riscv32-snitch-llvm-almalinux8-15.0.0-snitch-0.5.0/bin
